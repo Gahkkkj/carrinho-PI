61296b19e10fca90c67defdd38befef6d6e415cb
Gahkkkj <101150465+Gahkkkj@users.noreply.github.com>
2022-07-04 20:44:15 -0300

    Update index.php

--- carrinho/index.php
+++ carrinho/index.php
@@ -37,7 +37,8 @@ if ($res->num_rows > 0) {
     <?php include "navbar.php"; ?>
     <div class='container mt-5 pb-5'>
       <h2 class='text-muted mb-4 text-center' style="text-align: center;">Produtos</h2>
-      
+
+
       <div class='row'>
         <?php foreach ($data as $row) : ?>
           <div class='col-md-3 mt-2'>
@@ -49,28 +50,28 @@ if ($res->num_rows > 0) {
                   Preço R$ <?php echo $row["preco_produto"]; ?>
                 </p>
                 <a href="view_details.php?id=<?php echo $row["PID"]; ?>" class='btn btn-primary float-right' style="width: 47%">Comprar</a>
-                
-                
+
+
                 <div>
                   <a href="../editar.php?id=<?php echo $row["PID"]; ?>">
                     <button type='button' class='btn btn-primary float-left' style="width: 45%">Editar</button>
                   </a>
                 </div>
-<br>
-<br>
+                <br>
+                <br>
                 <a href="../excluir.php?id=<?php echo $row["PID"]; ?>">
                   <button type='button' class='btn btn-danger botoes' style="padding: 0.375rem 5rem; text-align: center;">Excluir</button>
                 </a>
-<div style="margin-top: 5%; height: 10%;">
-                <a href="https://wa.me/5551995665319" style="margin-top: 25px;">
-                  <button type="button" class="btn btn-success" style="padding: 0.6rem 5.9rem; text-align: center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-                      <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
-                    </svg>
+                <div style="margin-top: 5%; height: 10%;">
+                  <a href="https://wa.me/5551995665319" style="margin-top: 25px;">
+                    <button type="button" class="btn btn-success" style="padding: 0.6rem 5.9rem; text-align: center">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                        <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+                      </svg>
 
-                  </button>
-                </a>
-</div>
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
@@ -83,10 +84,10 @@ if ($res->num_rows > 0) {
 
   </footer>
   </section class="fixe">
-  <footer class="pt-4 pt-md-5 mt-5 border-top bg-dark" id="futer">
+  <footer class="pt-4 pt-md-5 mt-5 border-top bg-dark" style="text-align: center;">
     <div class="row andrei">
       <div class="col-4">
-        <h5 class="andreifooter"> MAIS </h5>
+        <h5 class="andreifooter" style="text-align: center; color: orange;"> MAIS </h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Algo legal</a></li>
           <li><a class="text-muted" href="#">Feature aleatória</a></li>
@@ -97,7 +98,7 @@ if ($res->num_rows > 0) {
         </ul>
       </div>
       <div class="col-4">
-        <h5 class="andreifooter"> FONTES </h5>
+        <h5 class="andreifooter" style="text-align: center; color: orange;"> FONTES </h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Fonte</a></li>
           <li><a class="text-muted" href="#">Nome da fonte</a></li>
@@ -106,7 +107,7 @@ if ($res->num_rows > 0) {
         </ul>
       </div>
       <div class="col-4">
-        <h5 class="andreifooter"> SOBRE </h5>
+        <h5 class="andreifooter" style="text-align: center; color: orange;"> SOBRE </h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-muted" href="#">Equipe</a></li>
           <li><a class="text-muted" href="#">Locais</a></li>
