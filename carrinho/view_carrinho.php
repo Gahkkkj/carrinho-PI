dddbbbcf5d8ce39d0d2eaf5fc90763540fa14da3
Gahkkkj <gabrieldasilvapereira300@outlook.com>
2022-07-26 23:09:54 -0300

    carrinho

--- carrinho/view_carrinho.php
+++ carrinho/view_carrinho.php
@@ -26,14 +26,15 @@ $cart = new Cart();
 		<div class="container">
 			<div class="row">
 				<div class="d-flex col-8">
-					<div class="bg-dark p-2 flex-grow-1  ">
-						<h2 class='mb-4 text-center ' style="color:orange ">Itens no carrinho</h2>
+					<div class="bg-dark p-4">
+						<h2 class='mb-4 text-center' style="color:orange ">Itens no carrinho</h2>
 						<?php if ($cart->get_cart_total() > 0) : ?>
 							<table class='table flex-shrink-1  table-grid table-striped table-responsive-mb table-bordered table-hover table-dark'>
 								<thead>
 									<tr>
-										<th colspan='2' class='text-center'>Produtos</th>
-										<th>preço do produto</th>
+										<th colspan='2' class='text-center'>Produto</th>
+
+										<th>preço</th>
 										<th>Quantidade</th>
 										<th>Total</th>
 										<th>Ação</th>
@@ -43,12 +44,18 @@ $cart = new Cart();
 									<?php $items = $cart->get_all_items(); ?>
 									<?php foreach ($items as $item) : ?>
 										<tr>
-											<td><img src='images/2.jpg<?php echo $item["img"]; ?>' style='height:80px;'></td>
+											<td><img src='images/2.jpg<?php echo $item["img"]; ?>' style='height:120px;'></td>
 											<td><?php echo $item["name"]; ?></td>
 											<td> R$ <?php echo $item["preco_produto"]; ?></td>
 											<td><input type='number' value='<?php echo $item["qty"]; ?>' class='qty' pid='<?php echo $item["id"]; ?>' min='1'></td>
 											<td> R$ <span class='row_total'><?php echo $item["total"]; ?></span></td>
-											<td><button type="button" href='remove.php?id=<?php echo $item["id"]; ?>' onclick="return confirm('Certeza que quer remover')" class="btn-excluir fas fa-trash-alt"></button></td>
+											<td>
+												<a href='remove.php?id=<?php echo $item["id"]; ?>' onclick="return confirm('Certeza que quer remover')">
+													<button type="button" class="btn-excluir fas fa-trash-alt"></button>
+												</a>
+											</td>
+
+
 										</tr>
 									<?php endforeach; ?>
 								</tbody>
@@ -61,7 +68,7 @@ $cart = new Cart();
 					<div class="col-5">
 						<div class="cart-summary">
 							<div style="font-weight: 900; ">
-								<div class="text-center" style="padding: 0px 2px; font-size: 24px; color: rgb(0, 0, 0);">Total</div>
+								<div class="text-center" style="font-size: 24px; color: rgb(0, 0, 0);">Total</div>
 								<div class=" font-weight-bold text-center" style=" font-size: 20px;">Total: <span>R$<span id='total'><?php echo $cart->get_cart_total(); ?></span></div>
 								<a href='checkout.php'>
 									<button type="button" class='btn-Comprar'>Comprar<span> (<?php echo $cart->get_cart_count(); ?>)</span></button>
